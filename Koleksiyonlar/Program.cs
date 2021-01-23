40d5b714bf981fe8b76eb76886e5971f4f6ad8b8
hp <hp@LAPTOP-2NCLCCA8>
2021-01-23 15:36:30 +0300

    20 ocak ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> {"Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("Ilker");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+
+        }
+    }
+}
