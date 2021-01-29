26eaa5edec03cd505df45c2173b81ecbc6366a60
hp <hp@LAPTOP-2NCLCCA8>
2021-01-29 15:50:12 +0300

    23 ocak ders sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metin Keskin
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "12344";
+            musteri1.Adi = "Metin";
+            musteri1.Soyadi="Keskin";
+            musteri1.TcNo = "123454556";
+
+            //keskin ticaret
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "12345644789";
+            musteri2.SirketAdi = "Keskin ticaret";
+            musteri2.VergiNo = "546321";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri1);
+
+        }
+    }
+}
---
+++ OOP3/FİleLoggerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP3
+{
+    class FİleLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Dosyaya loglandı");
+        }
+    }
+}
